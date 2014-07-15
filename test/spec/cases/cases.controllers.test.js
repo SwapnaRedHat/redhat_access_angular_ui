a26b6d9527f199c13ac341a7717fa6ea012b22f7
Swapna Gadkari <sgadkari@sgadkari.usersys.redhat.com>
2014-07-15 17:48:06 +0530

    Test cases for case  controllers

--- test/spec/cases/cases.controllers.test.js
+++ test/spec/cases/cases.controllers.test.js
@@ -2,112 +2,178 @@
 
 describe('Case Controllers', function() {
 
-		var caseService;
-		var strataService;
-		var recommendationsService;
-		var mockScope;
-		var q;
+	var mockScope;
+	var attachmentsService;
+	var securityService;
+	var caseService;
+	var treeViewSelectorUtils;
+	var alertService;
+	var strataService;
+	var q;
 
-		beforeEach(angular.mock.module('RedhatAccess.cases'));
-		beforeEach(function () {
-				inject(function ($injector, $rootScope, $q) {
-						caseService = $injector.get('CaseService');
-						q = $q;
-						strataService = $injector.get('strataService');
-						recommendationsService = $injector.get('RecommendationsService');
-						mockScope = $rootScope.$new();					
-				})
+	beforeEach(angular.mock.module('RedhatAccess.cases'));
+	beforeEach(function () {
+		inject(function ($injector, $rootScope, $q) {
+			attachmentsService = $injector.get('AttachmentsService');
+			securityService = $injector.get('securityService');
+			strataService = $injector.get('strataService');
+			alertService = $injector.get('AlertService');
+			caseService = $injector.get('CaseService');
+			mockScope = $rootScope.$new();
+			q = $q;
+		});
+	});
+
+	it('should have no file chosen with no description', inject(function ($controller) {
+
+		$controller('AttachLocalFile', {
+			$scope: mockScope,
+			AttachmentsService: attachmentsService,
+			securityService: securityService
 		});
 
-		it('should have a function for updating case details', inject(function ($controller) {
+		expect(mockScope.NO_FILE_CHOSEN).toEqual('No file chosen');
+		expect(mockScope.fileDescription).toEqual('');
 
-				$controller('DetailsSection', {
-						$scope: mockScope,
-						CaseService: caseService,
-						strataService: strataService
-				});
+	}));
 
-				var caseJSON = {};
-				caseService.case.case_number = '1234';
-				expect(mockScope.updateCase).toBeDefined();
-				var deferred = q.defer();
-				spyOn(strataService.cases, 'put').andReturn(deferred.promise);
-		        deferred.resolve();
-		        mockScope.updateCase();
-		        expect(strataService.cases.put).toHaveBeenCalledWith("1234", caseJSON);
-				
-		}));
+	it('should clear filename and file description', inject(function ($controller) {
+		$controller('AttachLocalFile', {
+			$scope: mockScope,
+			AttachmentsService: attachmentsService,
+			securityService: securityService
+		});
 
-		it('should have a function for adding comments to case', inject(function ($controller) {
+		expect(mockScope.clearSelectedFile).toBeDefined();
+		expect(mockScope.fileName).toEqual('No file chosen');
+		expect(mockScope.fileDescription).toEqual('');
+	}));
 
-				$controller('AddCommentSection', {
-						$scope: mockScope,
-						CaseService: caseService,
-						strataService: strataService
-				});
+	it('should get selected file', inject(function ($controller) {
+		$controller('AttachLocalFile', {
+			$scope: mockScope,
+			AttachmentsService: attachmentsService,
+			securityService: securityService
+		});
 
-				caseService.case.case_number = '1234';
-				caseService.commentText = 'test comment';
-				expect(mockScope.addComment).toBeDefined();
-				var deferred = q.defer();
-				spyOn(strataService.cases.comments, 'post').andReturn(deferred.promise);
-		        deferred.resolve();
-				mockScope.addComment();
-				expect(strataService.cases.comments.post).toHaveBeenCalledWith("1234", 'test comment');			
+		expect(mockScope.selectFile).toBeDefined();
 
-		
-		}));
+	}));
 
-		it('should have a function for adding draft comments to case', inject(function ($controller) {
+	it('should add file to the list of attachments', inject(function ($controller) {
+		$controller('AttachLocalFile', {
+			$scope: mockScope,
+			AttachmentsService: attachmentsService,
+			securityService: securityService
+		});
 
-				$controller('AddCommentSection', {
-						$scope: mockScope,
-						CaseService: caseService,
-						strataService: strataService
-				});
+		mockScope.fileObj = 'test_data';
+		mockScope.fileDescription = 'test_description';
+		mockScope.fileName = 'test_file';
+		mockScope.fileSize = '200MB';
 
-				caseService.case.case_number = '1234';
-				caseService.commentText = 'test comment';
-				caseService.draftComment = {};
-				caseService.draftComment.id = '1111';
-				expect(mockScope.addComment).toBeDefined();
-				var deferred = q.defer();
-				spyOn(strataService.cases.comments, 'put').andReturn(deferred.promise);
-		        deferred.resolve();
-				mockScope.addComment();
-				expect(strataService.cases.comments.put).toHaveBeenCalledWith("1234", 'test comment', false, '1111');						
+		expect(mockScope.addFile).toBeDefined();
+		mockScope.addFile();
+		expect(attachmentsService.updatedAttachments.length).toEqual(1);
 
-		
-		}));
+	}));
 
-		it('should have a function for fetching recommendations', inject(function ($controller) {
+	it('should have status of cases as open', inject(function ($controller) {
+		$controller('StatusSelect', {
+			$scope: mockScope,
+			securityService: securityService,
+			CaseService: caseService,
+			STATUS: 'Open'
+		});
 
-				$controller('New', {
-						$scope: mockScope,
-						RecommendationsService: recommendationsService,
-						strataService: strataService
-				});
-
-				var newData = {
-			        product: 'Red Hat Enterprise Linux',
-			        version: '6.0',
-			        summary: 'test case summary',
-			        description: 'test case description'		        
-      			};
-
-      			caseService.case.product = 'Red Hat Enterprise Linux';
-		        caseService.case.version = '6.0';
-		        caseService.case.summary = 'test case summary';
-		        caseService.case.description = 'test case description';
-
-      			expect(mockScope.getRecommendations).toBeDefined();
-      			var deferred = q.defer();
-				spyOn(strataService, 'problems').andReturn(deferred.promise);
-		        deferred.resolve();
-				mockScope.getRecommendations();
-				expect(strataService.problems).toHaveBeenCalledWith(newData,5);						
-
-		
-		}));
+		expect(mockScope.STATUS).toEqual('Open');
+		expect(mockScope.statuses[0].name).toEqual('Open and Closed');
+		expect(mockScope.statuses[1].name).toEqual('Open');
+		expect(mockScope.statuses[2].name).toEqual('Closed');
+	}));
 	
+	it('should have status of cases as closed', inject(function ($controller) {
+		$controller('StatusSelect', {
+			$scope: mockScope,
+			securityService: securityService,
+			CaseService: caseService,
+			STATUS: 'Closed'
+		});
+
+		expect(mockScope.STATUS).toEqual('Closed');
+		expect(mockScope.statuses[0].name).toEqual('Open and Closed');
+		expect(mockScope.statuses[1].name).toEqual('Open');
+		expect(mockScope.statuses[2].name).toEqual('Closed');
+	}));
+
+	it('should have cases with status as Open and Closed', inject(function ($controller) {
+		$controller('StatusSelect', {
+			$scope: mockScope,
+			securityService: securityService,
+			CaseService: caseService,
+			STATUS: 'Open and Closed'
+		});
+
+		expect(mockScope.STATUS).toEqual('Open and Closed');
+		expect(mockScope.statuses[0].name).toEqual('Open and Closed');
+		expect(mockScope.statuses[1].name).toEqual('Open');
+		expect(mockScope.statuses[2].name).toEqual('Closed');
+	}));
+
+	it('shoud not export all cases as CSV', inject(function ($controller) {
+		$controller('ExportCSVButton', {
+			$scope: mockScope,
+			strataService: strataService,
+			AlertService: alertService
+		});
+
+		expect(mockScope.exporting).toEqual(false);
+	}));
+
+	it('shoud export all cases as CSV', inject(function ($controller) {
+		$controller('ExportCSVButton', {
+			$scope: mockScope,
+			strataService: strataService,
+			AlertService: alertService
+		});
+		expect(mockScope.export).toBeDefined();
+		var deferred = q.defer();
+		spyOn(strataService.cases, 'csv').andReturn(deferred.promise);
+		deferred.resolve();
+		mockScope.export();
+		expect(strataService.cases.csv).toHaveBeenCalledWith();
+	}));
+
+	it('should select user account', inject(function ($controller) {
+		$controller('AccountSelect', {
+			$scope: mockScope,
+			strataService: strataService,
+			AlertService: alertService,
+			CaseService: caseService
+		});
+
+		expect(mockScope.selectUserAccount).toBeDefined();
+		var deferred = q.defer();
+		spyOn(strataService.accounts, 'list').andReturn(deferred.promise);
+		deferred.resolve();
+		mockScope.selectUserAccount();
+		expect(strataService.accounts.list).toHaveBeenCalledWith();
+	}));
+
+	it('should populate account specific fields', inject(function ($controller) {
+		$controller('AccountSelect', {
+			$scope: mockScope,
+			strataService: strataService,
+			AlertService: alertService,
+			CaseService: caseService
+		});
+
+		expect(mockScope.populateAccountSpecificFields).toBeDefined();
+		caseService.account.number = 1234;
+		var deferred = q.defer();
+		spyOn(strataService.accounts, 'get').andReturn(deferred.promise);
+		deferred.resolve();
+		mockScope.populateAccountSpecificFields();
+		expect(strataService.accounts.get).toHaveBeenCalledWith(1234);
+	}));
 });
